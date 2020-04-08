8298245791e4ba65e0a1f5ee9b9e5eceeac08752
Paul Cockrell <paulcockrell@gmail.com>
2020-04-08 03:18:01 +0100

    Update repo with final version

--- astrocalc-app.js
+++ astrocalc-app.js
@@ -1,32 +1,115 @@
 /**
- * Please note when developing:
- * IDE settings:
- * Esprima: Minification = TRUE
- * Esprima: Mangle = FALSE
- */
-
-/**
  * Inspired by: https://www.timeanddate.com
  */
 
-/**
- * SunCalc modified to work with Espruino by Paul Cockrell 2020
- */
+const SunCalc = require("suncalc.js");
 
-/** 
- * Original SunCalc library lisence:
- * 
- * (c) 2011-2015, Vladimir Agafonkin
- * SunCalc is a JavaScript library for calculating sun/moon position and light phases.
- * https://github.com/mourner/suncalc
-*/
-!function(){"use strict";var n=Math.PI,t=Math.sin,e=Math.cos,r=Math.tan,a=Math.asin,u=Math.atan2,o=Math.acos,i=n/180,c=864e5,s=2440588,d=2451545;function f(n){return new Date((n+.5-s)*c)}function l(n){return function(n){return n.valueOf()/c-.5+s}(n)-d}var h=23.4397*i;function M(n,a){return u(t(n)*e(h)-r(a)*t(h),e(n))}function g(n,r){return a(t(r)*e(h)+e(r)*t(h)*t(n))}function v(n,a,o){return u(t(n),e(n)*t(a)-r(o)*e(a))}function w(n,r,u){return a(t(r)*t(u)+e(r)*e(u)*e(n))}function m(n,t){return i*(280.16+360.9856235*n)-t}function p(n){return i*(357.5291+.98560028*n)}function D(e){return e+i*(1.9148*t(e)+.02*t(2*e)+3e-4*t(3*e))+102.9372*i+n}function P(n){var t=D(p(n));return{dec:g(t,0),ra:M(t,0)}}var b={getPosition:function(n,t,e){var r=i*-e,a=i*t,u=l(n),o=P(u),c=m(u,r)-o.ra;return{azimuth:v(c,a,o.dec),altitude:w(c,a,o.dec)}}},y=b.times=[[-.833,"sunrise","sunset"],[-.3,"sunriseEnd","sunsetStart"],[-6,"dawn","dusk"],[-12,"nauticalDawn","nauticalDusk"],[-18,"nightEnd","night"],[6,"goldenHourEnd","goldenHour"]];b.addTime=function(n,t,e){y.push([n,t,e])};var H=9e-4;function T(t,e,r){return H+(t+e)/(2*n)+r}function E(n,e,r){return d+n+.0053*t(e)-.0069*t(2*r)}function I(n,r,a,u,i,c,s){return E(T(function(n,r,a){return o((t(n)-t(r)*t(a))/(e(r)*e(a)))}(n,a,u),r,i),c,s)}function k(n){var r=i*(134.963+13.064993*n),a=i*(93.272+13.22935*n),u=i*(218.316+13.176396*n)+6.289*i*t(r),o=5.128*i*t(a),c=385001-20905*e(r);return{ra:M(u,o),dec:g(u,o),dist:c}}function q(n,t){return new Date(n.valueOf()+t*c/24)}b.getTimes=function(t,e,r,a){var u,o,s,d,h,M=i*-r,v=i*e,w=function(n){return-2.076*Math.sqrt(n)/60}(a=a||0),m=function(t,e){return Math.round(t-H-e/(2*n))}(l(t),M),P=T(0,M,m),b=p(P),k=D(b),q=g(k,0),x=E(P,b,k),z={solarNoon:f(x),nadir:f(x-.5)};for(u=0,o=y.length;u<o;u+=1)h=x-((d=I(((s=y[u])[0]+w)*i,M,v,q,m,b,k))-x),z[s[1]]=new Date(f(h)-c/2),z[s[2]]=new Date(f(d)+c/2);return z},b.getMoonPosition=function(n,a,o){var c=i*-o,s=i*a,d=l(n),f=k(d),h=m(d,c)-f.ra,M=w(h,s,f.dec),g=u(t(h),r(s)*e(f.dec)-t(f.dec)*e(h));return M+=function(n){return n<0&&(n=0),2967e-7/Math.tan(n+.00312536/(n+.08901179))}(M),{azimuth:v(h,s,f.dec),altitude:M,distance:f.dist,parallacticAngle:g}},b.getMoonIllumination=function(n){var r=l(n||new Date),a=P(r),i=k(r),c=o(t(a.dec)*t(i.dec)+e(a.dec)*e(i.dec)*e(a.ra-i.ra)),s=u(149598e3*t(c),i.dist-149598e3*e(c)),d=u(e(a.dec)*t(a.ra-i.ra),t(a.dec)*e(i.dec)-e(a.dec)*t(i.dec)*e(a.ra-i.ra));return{fraction:(1+e(s))/2,phase:.5+.5*s*(d<0?-1:1)/Math.PI,angle:d}},b.getMoonTimes=function(n,t,e,r){var a=new Date(n);r?a.setUTCHours(0,0,0,0):a.setHours(0,0,0,0);for(var u,o,c,s,d,f,l,h,M,g,v,w,m,p=.133*i,D=b.getMoonPosition(a,t,e).altitude-p,P=1;P<=24&&(u=b.getMoonPosition(q(a,P),t,e).altitude-p,h=((d=(D+(o=b.getMoonPosition(q(a,P+1),t,e).altitude-p))/2-u)*(l=-(f=(o-D)/2)/(2*d))+f)*l+u,g=0,(M=f*f-4*d*u)>=0&&(v=l-(m=Math.sqrt(M)/(2*Math.abs(d))),w=l+m,Math.abs(v)<=1&&g++,Math.abs(w)<=1&&g++,v<-1&&(v=w)),1===g?D<0?c=P+v:s=P+v:2===g&&(c=P+(h<0?w:v),s=P+(h<0?v:w)),!c||!s);P+=2)D=o;var y={};return c&&(y.rise=q(a,c)),s&&(y.set=q(a,s)),c||s||(y[h>0?"alwaysUp":"alwaysDown"]=!0),y},"object"==typeof exports&&"undefined"!=typeof module?module.exports=b:"function"==typeof define&&define.amd?define(b):global.SunCalc=b}();
+function drawMoon(phase, x, y) {
+  const moonImgFiles = [
+    "new",
+    "waxing-crescent",
+    "first-quarter",
+    "waxing-gibbous",
+    "full",
+    "waning-gibbous",
+    "last-quarter",
+    "waning-crescent",
+  ];
+
+  img = require("Storage").read(`${moonImgFiles[phase]}.img`);
+  // image width & height = 92px
+  g.drawImage(img, x - parseInt(92 / 2), y);
+}
+
+// linear interpolation between two values a and b
+// u controls amount of a/b and is in range [0.0,1.0]
+function lerp(a,b,u) {
+  return (1-u) * a + u * b;
+}
+
+function titlizeKey(key) {
+  return (key[0].toUpperCase() + key.slice(1)).match(/[A-Z][a-z]+/g).join(" ");
+}
+
+function dateToTimeString(date) {
+  const hrs = ("0" + date.getHours()).substr(-2);
+  const mins = ("0" + date.getMinutes()).substr(-2);
+  const secs = ("0" + date.getMinutes()).substr(-2);
+
+  return `${hrs}:${mins}:${secs}`;
+}
+
+function drawTitle(key) {
+  const fontHeight = 16;
+  const x = 0;
+  const x2 = g.getWidth() - 1;
+  const y = fontHeight + 26;
+  const y2 = g.getHeight() - 1;
+  const title = titlizeKey(key);
+
+  g.setFont("6x8", 2);
+  g.setFontAlign(0,-1);
+  g.drawString(title,(x+x2)/2,y-fontHeight-2);
+  g.drawLine(x,y-2,x2,y-2);
+}
+
+/**
+ * @params {Number} angle Angle of point around a radius
+ * @params {Number} radius Radius of the point to be drawn, default 2
+ * @params {Object} color Color of the point
+ * @params {Number} color.r Red 0-1
+ * @params {Number} color.g Green 0-1
+ * @params {Number} color.b Blue 0-1
+ */
+function drawPoint(angle, radius, color) {
+  const pRad = Math.PI / 180;
+  const faceWidth = 80; // watch face radius
+  const centerPx = g.getWidth() / 2;
+
+  const a = angle * pRad;
+  const x = centerPx + Math.sin(a) * faceWidth;
+  const y = centerPx - Math.cos(a) * faceWidth;
+
+  if (!radius) radius = 2;
+
+  g.setColor(color.r, color.g, color.b);
+  g.fillCircle(x, y + 20, radius);
+}
+
+function drawPoints() {
+  const startColor = {r: 140, g: 255, b: 255};  // light blue
+  const endColor   = {r: 0, g: 0, b: 140};  // dark turquoise
+
+  const steps = 60;
+  const step_u = 1.0 / (steps / 2);
+  let u = 0.0;
+
+  for (let i = 0; i < steps; i++) {
+    const colR = lerp(startColor.r, endColor.r, u) / 255;
+    const colG = lerp(startColor.g, endColor.g, u) / 255;
+    const colB = lerp(startColor.b, endColor.b, u) / 255;
+    const col = {r: colR, g: colG, b: colB};
+
+    if (i >= 0 && i <= 30) {
+      u += step_u;
+    } else {
+      u -= step_u;
+    }
+
+    drawPoint((360 * i) / steps, 2, col);
+  }
+}
 
 function drawData(title, obj, startX, startY) {
+  g.clear();
+  drawTitle(title);
+
   let xPos, yPos;
 
-  if (typeof(startX) === "undefined") {
-    xPos = 5;
+  if (typeof(startX) === "undefined" || startX === null) {
+    // Center text
+    g.setFontAlign(0,-1);
+    xPos = (0 + g.getWidth() - 2) / 2;
   } else {
     xPos = startX;
   }
@@ -37,10 +120,8 @@ function drawData(title, obj, startX, startY) {
     yPos = startY;
   }
 
-  g.clear();
   g.setFont("6x8", 1);
 
-  g.drawString(title, xPos, yPos);
   Object.keys(obj).forEach((key) => {
     g.drawString(`${key}: ${obj[key]}`, xPos, yPos += 20);
   });
@@ -48,20 +129,66 @@ function drawData(title, obj, startX, startY) {
   g.flip();
 }
 
-function drawMoonPositionPage(gps) {
+function drawMoonPositionPage(gps, title) {
   const pos = SunCalc.getMoonPosition(new Date(), gps.lat, gps.lon);
 
-  drawData("Moon - Position", pos);
+  const pageData = {
+    Azimuth: pos.azimuth.toFixed(2),
+    Altitude: pos.altitude.toFixed(2),
+    Distance: `${pos.distance.toFixed(0)} km`,
+    "Parallactic Ang": pos.parallacticAngle.toFixed(2),
+  };
+  const azimuthDegrees = parseInt(pos.azimuth * 180 / Math.PI);
+
+  drawData(title, pageData, null, 80);
+  drawPoints();
+  drawPoint(azimuthDegrees, 8, {r: 1, g: 1, b: 1});
 
   let m = setWatch(() => {
       let m = moonIndexPageMenu(gps);
   }, BTN3, {repeat: false, edge: "falling"});
 }
 
-function drawMoonIlluminationPage(gps) {
-  const illum = SunCalc.getMoonIllumination(new Date());
+function drawMoonIlluminationPage(gps, title) {
+  const phaseNames = [
+    "New Moon", "Waxing Crescent", "First Quarter", "Waxing Gibbous",
+    "Full Moon", "Waning Gibbous", "Last Quater", "Waning Crescent",
+  ];
 
-  drawData("Moon - Illumination", illum);
+  const phase = SunCalc.getMoonIllumination(new Date());
+  const pageData = {
+    Phase: phaseNames[phase.phase],
+  };
+
+  drawData(title, pageData, null, 35);
+  drawMoon(phase.phase, g.getWidth() / 2, g.getHeight() / 2);
+
+  let m = setWatch(() => {
+    let m = moonIndexPageMenu(gps);
+  }, BTN3, {repease: false, edge: "falling"});
+}
+
+
+function drawMoonTimesPage(gps, title) {
+  const times = SunCalc.getMoonTimes(new Date(), gps.lat, gps.lon);
+
+  const pageData = {
+    Rise: dateToTimeString(times.rise),
+    Set: dateToTimeString(times.set),
+  };
+
+  drawData(title, pageData, null, 105);
+  drawPoints();
+
+  // Draw the moon rise position
+  const risePos = SunCalc.getMoonPosition(times.rise, gps.lat, gps.lon);
+  const riseAzimuthDegrees = parseInt(risePos.azimuth * 180 / Math.PI);
+  drawPoint(riseAzimuthDegrees, 8, {r: 1, g: 1, b: 1});
+
+  // Draw the moon set position
+  const setPos = SunCalc.getMoonPosition(times.set, gps.lat, gps.lon);
+  const setAzimuthDegrees = parseInt(setPos.azimuth * 180 / Math.PI);
+  drawPoint(setAzimuthDegrees, 8, {r: 1, g: 1, b: 1});
 
   let m = setWatch(() => {
     let m = moonIndexPageMenu(gps);
@@ -70,8 +197,29 @@ function drawMoonIlluminationPage(gps) {
 
 function drawSunShowPage(gps, key, date) {
   const pos = SunCalc.getPosition(date, gps.lat, gps.lon);
-  
-  drawData(`Sun - ${key}`, pos);
+
+  const hrs = ("0" + date.getHours()).substr(-2);
+  const mins = ("0" + date.getMinutes()).substr(-2);
+  const secs = ("0" + date.getMinutes()).substr(-2);
+  const time = `${hrs}:${mins}:${secs}`;
+
+  const azimuth = Number(pos.azimuth.toFixed(2));
+  const azimuthDegrees = parseInt(pos.azimuth * 180 / Math.PI);
+  const altitude = Number(pos.altitude.toFixed(2));
+
+  const pageData = {
+    Time: time,
+    Altitude: altitude,
+    Azimumth: azimuth,
+    Degrees: azimuthDegrees
+  };
+
+  drawData(key, pageData, null, 85);
+
+  drawPoints();
+
+  // Draw the suns position
+  drawPoint(azimuthDegrees, 8, {r: 1, g: 1, b: 0});
 
   m = setWatch(() => {
       m = sunIndexPageMenu(gps);
@@ -87,16 +235,22 @@ function sunIndexPageMenu(gps) {
       "": {
         "title": "-- Sun --",
       },
-      "< Back": () => m = indexPageMenu(gps),
+      "Current Pos": () => {
+        m = E.showMenu();
+        drawSunShowPage(gps, "Current Pos", new Date());
+      },
     };
 
     Object.keys(sunTimes).sort().reduce((menu, key) => {
-      menu[key] = () => {
+      const title = titlizeKey(key);
+      menu[title] = () => {
         m = E.showMenu();
         drawSunShowPage(gps, key, sunTimes[key]);
       };
       return menu;
     }, sunMenu);
+
+    sunMenu["< Back"] = () => m = indexPageMenu(gps);
 
     return E.showMenu(sunMenu);
 }
@@ -107,13 +261,17 @@ function moonIndexPageMenu(gps) {
       "": {
         "title": "-- Moon --",
       },
+      "Times": () => {
+        m = E.showMenu();
+        drawMoonTimesPage(gps, "Times");
+      },
       "Position": () => {
         m = E.showMenu();
-        drawMoonPositionPage(gps);
+        drawMoonPositionPage(gps, "Position");
       },
       "Illumination": () => {
         m = E.showMenu();
-        drawMoonIlluminationPage(gps);
+        drawMoonIlluminationPage(gps, "Illumination");
       },
       "< Back": () => m = indexPageMenu(gps),
     };
@@ -142,13 +300,17 @@ function indexPageMenu(gps) {
  * GPS wait page, shows GPS locating animation until it gets a lock, then moves to the Sun page
  */
 function drawGPSWaitPage() {
+    const img = require("heatshrink").decompress(atob("mEwxH+AH4A/AH4AW43GF1wwsFwYwqFwowoFw4wmFxIwdE5YAPF/4vM5nN6YAE5vMF8YtHGIgvhFpQxKF7AuOGA4vXFyAwGF63MFyIABF6xeWMC4UDLwvNGpAJG5gwSdhIIDRBLyWCIgcJHAgJJDoouQF4vMQoICBBJoeGFx6GGACIfHL6YvaX6gvZeCIdFc4gAFXogvGFxgwFDwovQCAguOGAnMMBxeG5guTGAggGGAwNKFySREcA3N5vM5gDBdpQvXEY4AKXqovGGCKbFF7AwPZQwvZGJgtGF7vGdQItG5gSIF7gASF/44WEzgwRF0wwHF1AwFF1QwDF1gvwAH4A/AFAA=="))
+   
     g.clear();
+    g.drawImage(img, 100, 50);
     g.setFont("6x8", 1);
-    g.drawString("Locating GPS", 85, 100);
-    g.drawString("Please wait...", 80, 115);
+    g.drawString("Astrocalc v0.01", 80, 105);
+    g.drawString("Locating GPS", 85, 140);
+    g.drawString("Please wait...", 80, 155);
     g.flip();
 
-    const DEBUG = true;
+    const DEBUG = false;
     if (DEBUG) {
       const gps = {
         "lat": 56.45783133333,
